06f4c2b6c6087daec31e041918288fa7c06d1a89
Aaron Chu <aaronchu357@gmail.com>
2019-04-07 02:45:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |seasons|
-    seasons.capitalize!
+  holiday_hash.each do |seasons, holidays|
+    "#{seasons.capitalize!}:"
 
 
   end
